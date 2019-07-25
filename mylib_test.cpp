9256d08ac14903f198002e8b9a75d721dbf71392
Anandraj Govindan <anandrag@microsoft.com>
2019-07-25 15:05:10 -0700

    mylib implementation

---
+++ mylib_test.cpp
@@ -0,0 +1,13 @@
+﻿#include "mylib_test.h"
+
+using namespace std;
+
+class MyUtilsImpl : public IMyUtils {
+  int getToken() override {
+    return 101;
+  }
+};
+
+std::unique_ptr<IMyUtils> createMyUtils() {
+  return std::make_unique<MyUtilsImpl>();
+}
---
+++ mylib_test.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <memory>
+
+struct IMyUtils {
+  virtual int getToken() = 0;
+};
+
+std::unique_ptr<IMyUtils> createMyUtils();
